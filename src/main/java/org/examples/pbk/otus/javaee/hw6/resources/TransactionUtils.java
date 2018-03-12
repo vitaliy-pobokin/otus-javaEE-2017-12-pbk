c6b7a4a52f270f1c89ac09c526786149b3de45dd
Unknown <pbk.vitaliy@gmail.com>
2018-03-13 01:41:00 +0300

    Fix TransactionUtils to use Session

--- src/main/java/org/examples/pbk/otus/javaee/hw6/resources/TransactionUtils.java
+++ src/main/java/org/examples/pbk/otus/javaee/hw6/resources/TransactionUtils.java
@@ -1,6 +1,7 @@
 package org.examples.pbk.otus.javaee.hw6.resources;
 
 import org.examples.pbk.otus.javaee.hw6.listener.AppContextListener;
+import org.hibernate.Session;
 
 import javax.persistence.EntityManager;
 import java.util.function.Consumer;
@@ -8,19 +9,19 @@ import java.util.function.Function;
 
 public class TransactionUtils {
     public static <R> R runInTransaction(Function<EntityManager, R> function) {
-        EntityManager em = AppContextListener.getEmf().createEntityManager();
-        em.getTransaction().begin();
-        R result = function.apply(em);
-        em.getTransaction().commit();
-        em.close();
+        Session session = AppContextListener.getSessionFactory().openSession();
+        session.getTransaction().begin();
+        R result = function.apply(session);
+        session.getTransaction().commit();
+        session.close();
         return result;
     }
 
     public static void runInTransactionWithoutResult(Consumer<EntityManager> consumer) {
-        EntityManager em = AppContextListener.getEmf().createEntityManager();
-        em.getTransaction().begin();
-        consumer.accept(em);
-        em.getTransaction().commit();
-        em.close();
+        Session session = AppContextListener.getSessionFactory().openSession();
+        session.getTransaction().begin();
+        consumer.accept(session);
+        session.getTransaction().commit();
+        session.close();
     }
 }
