7cbff20eddd4a220c1a3cb4ab82218c096d326f2
Vitaliy Pobokin <pbk.vitaliy@gmail.com>
2018-03-25 10:33:53 +0300

    Added message class, message.service, chat.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { MessageService } from './message.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
