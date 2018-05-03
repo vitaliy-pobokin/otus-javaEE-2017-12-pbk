f9ea526ce81f36f97f54668d818e8f2593026a3f
Vitaliy Pobokin <pbk.vitaliy@gmail.com>
2018-05-03 22:35:09 +0300

    Fixed refresh page behaviour on some pages

--- hw_7_statistic/src/main/java/org/examples/pbk/otus/javaee/hw7/BaseServlet.java
+++ hw_7_statistic/src/main/java/org/examples/pbk/otus/javaee/hw7/BaseServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = {"/about", "/news", "/scripts", "/our_employees", "/employees"})
+@WebServlet(urlPatterns = {"/about", "/news", "/scripts", "/our_employees", "/employees", "/departments", "/statistic"})
 public class BaseServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
